55445e0588929bdad742b5fffb726315fa54db2b
tadef <tadef@DESKTOP-F3LITGC>
2023-09-06 21:50:34 -0300

    Edit Cat Class and add basic template

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Cat michi = new Cat(35,"Juan","Negro");
+        Log.d(TAG, michi.getName());
+    }
+
+
+}
